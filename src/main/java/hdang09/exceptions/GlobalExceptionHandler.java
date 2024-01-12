cb9be819df344fe81ec8bce88a405e313cc49c1c
hdang09 <dangtranhai628@gmail.com>
2024-01-13 00:49:45 +0700

    ADD - Add handle exception

---
+++ src/main/java/hdang09/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package hdang09.exceptions;
+
+import hdang09.constants.ResponseStatus;
+import hdang09.entities.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response<Exception>> handleException(Exception ex) {
+        Response<Exception> response = new Response<>(ResponseStatus.ERROR, ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+}
---
+++ src/main/java/hdang09/exceptions/ValidationExceptionHandler.java
@@ -0,0 +1,31 @@
+package hdang09.exceptions;
+
+import hdang09.constants.ResponseStatus;
+import hdang09.entities.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
+@RestController
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Response<BindException>> handleValidationException(BindException ex) {
+        // Get all errors
+        List<FieldError> fieldErrors = ex.getFieldErrors();
+
+        // Get the first error message
+        FieldError firstError = fieldErrors.get(0);
+        String errorMessage = firstError.getDefaultMessage();
+
+        Response<BindException> response = new Response<>(ResponseStatus.ERROR, errorMessage, ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+}
